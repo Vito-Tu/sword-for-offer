acc2eaeed537f4541d0ae8941827c9d227bf8f4e
vito <vitotu@qq.com>
2019-06-18 15:14:04 +0800

    update to 29

--- debugfile.py
+++ debugfile.py
@@ -1,24 +1,20 @@
-class ListNode:
+class TreeNode:
     """创建链表类"""
     def __init__(self, x=None):
         self.val = x
-        self.next = None
+        self.left = None
+        self.right = None
 class Solution:
-    # 返回合并后列表
-    def Merge(self, pHead1, pHead2):
-        """递归比较每一个节点的值，并将较小的节点加入新的链表中"""
-        if pHead1 == None:
-            return pHead2
-        elif pHead2 == None:
-            return pHead1
-        pMergedHead = None
-        if pHead1.val < pHead2.val:
-            pMergedHead = pHead1
-            pMergedHead.next = self.Merge(pHead1.next, pHead2)
-        else:
-            pMergedHead = pHead2
-            pMergedHead.next = self.Merge(pHead1, pHead2.next)
-        return pMergedHead
+    def isSymmetricalCore(self, pRoot1, pRoot2):
+        if pRoot1 == None and pRoot2 == None:
+            return True
+        if pRoot1 == None or pRoot2 == None:
+            return False
+        if pRoot1.val != pRoot2.val:
+            return False
+        return self.isSymmetricalCore(pRoot1.left,pRoot2.right) and self.isSymmetricalCore(pRoot1.right, pRoot2.left)
+    def isSymmetrical(self, pRoot):
+        return self.isSymmetricalCore(pRoot, pRoot)
 
 
                 
