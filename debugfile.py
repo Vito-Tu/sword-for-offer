b207e925f70d6b153183cf64cb620eaffd6f65da
vito <vitotu@qq.com>
2019-06-21 15:55:23 +0800

    update 32

--- debugfile.py
+++ debugfile.py
@@ -1,30 +1,45 @@
 class TreeNode:
-    """创建链表类"""
-    def __init__(self, x=None):
+    def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
 class Solution:
-    def __init__(self):
-        self.m_data = []
-        self.m_min = []
-    def push(self, node):
-        self.m_data.append(node)
-        if len(self.m_min) == 0 or node < self.m_min[-1]:
-            self.m_min.append(node)
-        else:
-            self.m_min.append(self.m_min[-1])
-    def pop(self):
-        if len(self.m_min) > 0 and len(self.m_data) > 0:
-            self.m_data.pop(-1)
-            self.m_min.pop(-1)
-        else:
-            assert Exception("data is empty")
-    def top(self):
-        return self.m_data[-1]
-    def min(self):
-        return self.m_min[-1]
-
-
-
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, pRoot):
+        if not pRoot:
+            return []
+        result = [[]]
+        levels = [[],[]]
+        current = 0
+        next = 1
+        levels[current].append(pRoot)
+        while levels[0] or levels[1]:
+            pNode = levels[current].pop()
+            result[-1].append(pNode.val)
+            if current == 0:
+                if pNode.left:
+                    levels[next].append(pNode.left)
+                if pNode.right:
+                    levels[next].append(pNode.right)
+            else:
+                if pNode.right:
+                    levels[next].append(pNode.right)
+                if pNode.left:
+                    levels[next].append(pNode.left)
+            if not levels[current] and levels[next]:
+                result.append([])
+                current = 1 - current
+                next = 1 - next
+        return result
+# test code
+pRoot = TreeNode(8)
+pRoot.left = TreeNode(6)
+pRoot.right = TreeNode(10)
+pRoot.left.left = TreeNode(5)
+pRoot.left.right = TreeNode(7)
+pRoot.right.left = TreeNode(9)
+pRoot.right.right = TreeNode(11)
+a = Solution()
+print(a.Print(pRoot))
                 
