58c3c433deeac2bf384d85c0429b42307a8a47df
vito <vitotu@qq.com>
2019-06-22 13:11:39 +0800

    update 33

--- debugfile.py
+++ debugfile.py
@@ -5,41 +5,37 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    # 返回二维列表[[1,2],[4,5]]
-    def Print(self, pRoot):
-        if not pRoot:
-            return []
-        result = [[]]
-        levels = [[],[]]
-        current = 0
-        next = 1
-        levels[current].append(pRoot)
-        while levels[0] or levels[1]:
-            pNode = levels[current].pop()
-            result[-1].append(pNode.val)
-            if current == 0:
-                if pNode.left:
-                    levels[next].append(pNode.left)
-                if pNode.right:
-                    levels[next].append(pNode.right)
-            else:
-                if pNode.right:
-                    levels[next].append(pNode.right)
-                if pNode.left:
-                    levels[next].append(pNode.left)
-            if not levels[current] and levels[next]:
-                result.append([])
-                current = 1 - current
-                next = 1 - next
-        return result
+    def VerifySquenceOfBST(self, sequence):
+        if not sequence:
+            return False
+        root = sequence[-1]
+        i = 0
+        while i < len(sequence) - 1:
+            if sequence[i] > root:
+                break
+            i += 1
+        # for i in range(len(sequence) - 1):
+        #     if sequence[i] > root:
+        #         break
+        for j in range(i, len(sequence) - 1):
+            if sequence[j] < root:
+                return False
+        left = True
+        if i > 0:
+            left = self.VerifySquenceOfBST(sequence[:i])
+        right = True
+        if i < len(sequence) - 1:
+            right = self.VerifySquenceOfBST(sequence[i:-1])
+        return left and right
 # test code
-pRoot = TreeNode(8)
-pRoot.left = TreeNode(6)
-pRoot.right = TreeNode(10)
-pRoot.left.left = TreeNode(5)
-pRoot.left.right = TreeNode(7)
-pRoot.right.left = TreeNode(9)
-pRoot.right.right = TreeNode(11)
+# pRoot = TreeNode(8)
+# pRoot.left = TreeNode(6)
+# pRoot.right = TreeNode(10)
+# pRoot.left.left = TreeNode(5)
+# pRoot.left.right = TreeNode(7)
+# pRoot.right.left = TreeNode(9)
+# pRoot.right.right = TreeNode(11)
 a = Solution()
-print(a.Print(pRoot))
+pRoot = [4,6,7,5]
+print(a.VerifySquenceOfBST(pRoot))
                 
