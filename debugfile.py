6c3798874b5edd425c99df417384e31c085e2df4
vito <vitotu@qq.com>
2019-06-23 15:38:37 +0800

    update 34

--- debugfile.py
+++ debugfile.py
@@ -5,37 +5,33 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def VerifySquenceOfBST(self, sequence):
-        if not sequence:
-            return False
-        root = sequence[-1]
-        i = 0
-        while i < len(sequence) - 1:
-            if sequence[i] > root:
-                break
-            i += 1
-        # for i in range(len(sequence) - 1):
-        #     if sequence[i] > root:
-        #         break
-        for j in range(i, len(sequence) - 1):
-            if sequence[j] < root:
-                return False
-        left = True
-        if i > 0:
-            left = self.VerifySquenceOfBST(sequence[:i])
-        right = True
-        if i < len(sequence) - 1:
-            right = self.VerifySquenceOfBST(sequence[i:-1])
-        return left and right
+    # 返回二维列表，内部每个列表表示找到的路径
+    def FindPathCore(self,root, expectNumber, path, result):
+        path.append(root.val)
+        if sum(path) == expectNumber and (root.left == None and root.right == None):
+            #找到一个匹配的路径
+            result.append(path.copy())
+        if root.left != None:
+            self.FindPathCore(root.left,expectNumber,path, result)
+        if root.right != None:
+            self.FindPathCore(root.right,expectNumber,path,result)
+        path.pop()
+    def FindPath(self, root, expectNumber):
+        if not root:
+            return []
+        path = []
+        result = []
+        self.FindPathCore(root, expectNumber,path, result)
+        return result
 # test code
-# pRoot = TreeNode(8)
-# pRoot.left = TreeNode(6)
-# pRoot.right = TreeNode(10)
-# pRoot.left.left = TreeNode(5)
-# pRoot.left.right = TreeNode(7)
-# pRoot.right.left = TreeNode(9)
-# pRoot.right.right = TreeNode(11)
+pRoot = TreeNode(8)
+pRoot.left = TreeNode(6)
+pRoot.right = TreeNode(10)
+pRoot.left.left = TreeNode(5)
+pRoot.left.right = TreeNode(7)
+pRoot.right.left = TreeNode(9)
+pRoot.right.right = TreeNode(11)
 a = Solution()
-pRoot = [4,6,7,5]
-print(a.VerifySquenceOfBST(pRoot))
+# pRoot = [4,6,7,5]
+print(a.FindPath(pRoot,27))
                 
