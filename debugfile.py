6317bde291392db66deda6ace2d1e17e88ce04c0
vito <vitotu@qq.com>
2019-06-24 14:37:01 +0800

    update 35

--- debugfile.py
+++ debugfile.py
@@ -3,26 +3,50 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
 class Solution:
-    # 返回二维列表，内部每个列表表示找到的路径
-    def FindPathCore(self,root, expectNumber, path, result):
-        path.append(root.val)
-        if sum(path) == expectNumber and (root.left == None and root.right == None):
-            #找到一个匹配的路径
-            result.append(path.copy())
-        if root.left != None:
-            self.FindPathCore(root.left,expectNumber,path, result)
-        if root.right != None:
-            self.FindPathCore(root.right,expectNumber,path,result)
-        path.pop()
-    def FindPath(self, root, expectNumber):
-        if not root:
-            return []
-        path = []
-        result = []
-        self.FindPathCore(root, expectNumber,path, result)
-        return result
+    # 返回 RandomListNode
+    def CloneNodes(self,phead):
+        """在原有的链表基础上进行赋值，即将每个节点沿着next链复制一个"""
+        pNode = phead
+        while pNode != None:
+            pCloned = RandomListNode()
+            pCloned.label = pNode.label
+            pCloned.next = pNode.next
+            pCloned.random = None
+            pNode.next = pCloned
+            pNode = pCloned.next
+    def ConnetSiblingNodes(self, pHead):
+        """修复复制后链表节点的random链接"""
+        pNode = pHead
+        while pNode != None:
+            pClone = pNode.next
+            if pNode.random != None:
+                pClone.random = pNode.random.next
+            pNode = pClone.next
+    def ReconnectNodes(self, pHead):
+        """将复制好的链表拆分成两个链表，奇数位为原链表，偶数位为赋值好的链表"""
+        pNode = pHead
+        pCloneHead = None
+        pCloneNode = None
+        if pNode != None:
+            pCloneHead = pCloneNode = pNode.next
+            pNode.next = pCloneNode.next
+            pNode = pNode.next
+        while pNode != None:
+            pCloneNode.next = pNode.next
+            pCloneNode = pCloneNode.next
+            pNode.next = pCloneNode.next
+            pNode = pNode.next
+        return pCloneHead
+    def Clone(self, pHead):
+        self.CloneNodes(pHead)
+        self.ConnetSiblingNodes(pHead)
+        return self.ReconnectNodes(pHead)
 # test code
 pRoot = TreeNode(8)
 pRoot.left = TreeNode(6)
