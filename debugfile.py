a4ae73745b1a22c89f71cac6c64e0d61eefc4a79
vito <vitotu@qq.com>
2019-06-29 15:57:52 +0800

    update 40

--- debugfile.py
+++ debugfile.py
@@ -8,39 +8,31 @@ class RandomListNode:
         self.label = x
         self.next = None
         self.random = None
-class Solution:
-    def PermutationCore(self, ss, i):
-        result = []
-        if i >= len(ss) - 1:
-            result.append(''.join(ss))
-            return result
-        else:
-            for j in range(i, len(ss)):
-                if ss[j] == ss[i] and j != i:
-                    # 防止输入中有重复字符
-                    continue
-                temp = ss[j]
-                ss[j] = ss[i]
-                ss[i] = temp
-                result = result + self.PermutationCore(ss, i+1)
-                temp = ss[j]
-                ss[j] = ss[i]
-                ss[i] = temp
-        return result
 
-    def Permutation(self, ss):
-        if not ss:
-            return ''
-        return self.PermutationCore(list(ss), 0)
+
+class Solution:
+    """解法二：时间复杂度为O(nlogK)的算法，适合用来处理海量数据"""
+    def GetLeastNumbers_Solution(self, tinput, k):
+        if not tinput or len(tinput) < k:
+            return
+        result = []
+        for i in tinput:
+            if len(result) < k:
+                result.append(i)
+            else:
+                result.sort()
+                if i < result[-1]:
+                    result[-1] = i
+        return sorted(result)
 # test code
-pRoot = TreeNode(10)
-pRoot.left = TreeNode(6)
-pRoot.right = TreeNode(14)
-pRoot.left.left = TreeNode(4)
-pRoot.left.right = TreeNode(8)
-pRoot.right.left = TreeNode(12)
-pRoot.right.right = TreeNode(16)
+# pRoot = TreeNode(10)
+# pRoot.left = TreeNode(6)
+# pRoot.right = TreeNode(14)
+# pRoot.left.left = TreeNode(4)
+# pRoot.left.right = TreeNode(8)
+# pRoot.right.left = TreeNode(12)
+# pRoot.right.right = TreeNode(16)
 a = Solution()
 # pRoot = [4,6,7,5]
-print(a.Permutation('abc'))
+print(a.GetLeastNumbers_Solution([4,5,1,6,2,7,3,8], 8))
                 
