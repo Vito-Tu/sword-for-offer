ddb79e0d4738a6d795dbcaecd656f6cf7efc5165
vito <vitotu@qq.com>
2019-07-05 13:01:53 +0800

    update 44

--- debugfile.py
+++ debugfile.py
@@ -10,28 +10,32 @@ class RandomListNode:
         self.random = None
 
 class Solution:
-    def NumberOf1(self,strN):
-        """递归的计算含有1的数量"""
-        if not strN:
+    def beginNumber(self, digits):
+        if digits == 1:
             return 0
-        first = ord(strN[0]) - ord('0')
-        length = len(strN)
-        if length == 1 and first == 0:
-            return 0
-        if length == 1 and first > 0:
-            return 1
-        numFirstDigit = 0
-        if first > 1:
-            numFirstDigit = pow(10, length - 1)
-        elif first == 1:
-            numFirstDigit = int(''.join(strN[1:])) + 1
-        numOtherDigts = first*(length - 1)*pow(10, length - 2)
-        numRecursive = self.NumberOf1(strN[1:])
-        return numFirstDigit + numOtherDigts + numRecursive
-    def NumberOf1Between1AndN_Solution(self, n):
-        if n <= 0:
-            return 0
-        return self.NumberOf1(list(str(n)))
+        return pow(10, digits - 1)
+    def digitsAtIndex(self,index, digits):
+        number = self.beginNumber(digits) + index / digits
+        indexFromRight = digits - index % digits
+        for i in range(1,indexFromRight):
+            number /= 10
+        return int(number % 10)
+    def countOfIntegers(self, digits):
+        if digits == 1:
+            return 10
+        count = pow(10, digits - 1)
+        return 9*count
+    def digitsAtIndex_main(self, index):
+        if index < 0:
+            return -1
+        digits = 1
+        while True:
+            number = self.countOfIntegers(digits)
+            if index < number * digits:
+                return self.digitsAtIndex(index, digits)
+            index -= digits * number
+            digits += 1
+        return -1
 
 # test code
 # pRoot = TreeNode(10)
@@ -43,7 +47,5 @@ class Solution:
 # pRoot.right.right = TreeNode(16)
 a = Solution()
 # pRoot = [4,6,7,5]
-data = [5,2,3,4,1,6,7,0,8]
-for i in data:
-    a.Insert(i)
-    print(a.GetMedian())
+data = -1
+print(a.digitsAtIndex_main(data))
