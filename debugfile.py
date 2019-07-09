1f1f37fcf3be7e58f06c95fe341960c16a2fbcb3
vito <vitotu@qq.com>
2019-07-09 12:56:08 +0800

    update 48

--- debugfile.py
+++ debugfile.py
@@ -10,33 +10,9 @@ class RandomListNode:
         self.random = None
 
 class Solution:
-    """定义一种新的比较大小数字大小的方法，使用该方法对数组进行排序，同时考虑大数问题"""
-    def GetTranslationCount(self,number):
-        if number < 0:
-            return 0
-        strNumber = str(number)
-        length = len(strNumber)
-        counts = [None] * length
-        count = 0
-        for i in range(length - 1, -1, -1):
-            count = 0
-            if i < length - 1:
-                count = counts[i+1]
-            else:
-                count = 1
-            if i < length - 1:
-                digit1 = ord(strNumber[i]) - ord('0')
-                digit2 = ord(strNumber[i+1]) - ord('0')
-                converted = digit1 * 10 + digit2
-                if converted >= 10 and converted <= 25:
-                    if i < length - 2:
-                        count += counts[i+2]
-                    else:
-                        count += 1
-            counts[i] = count
-        return count
-
-
+    def GetUglyNumber_Solution(self, index):
+        pass
+    
 # test code
 # pRoot = TreeNode(10)
 # pRoot.left = TreeNode(6)
@@ -47,5 +23,5 @@ class Solution:
 # pRoot.right.right = TreeNode(16)
 a = Solution()
 # pRoot = [4,6,7,5]
-data = 26
-print(a.GetTranslationCount(data))
+data = 'arabcacfr'
+print(a.longestSubstringWithoutDuplication(data))
