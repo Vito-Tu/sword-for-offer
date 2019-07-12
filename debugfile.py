22ca90ee15e5feb937b6ca08b52880b383f22a9c
vito <vitotu@qq.com>
2019-07-12 12:25:31 +0800

    update 53

--- debugfile.py
+++ debugfile.py
@@ -9,8 +9,44 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def FindFirstCommonNode(self, pHead1, pHead2):
-        pass
+    def GetFirstK(self,data, k, start, end):
+        if start > end:
+            return -1
+        middleIndex = (start + end) // 2
+        middleData = data[middleIndex]
+        if middleData == k:
+            if (middleIndex > 0 and data[middleIndex - 1] != k) or middleIndex == 0:
+                return middleIndex
+            else:
+                end = middleIndex - 1
+        elif middleData > k:
+            end = middleIndex - 1
+        else:
+            start = middleIndex + 1
+        return self.GetFirstK(data, k, start, end)
+    def GetLastK(self, data, k, start, end):
+        if start > end:
+            return -1
+        middleIndex = (start + end) // 2
+        middleData = data[middleIndex]
+        if middleData == k:
+            if (middleIndex < len(data) - 1 and data[middleIndex + 1] != k) or middleIndex == len(data) - 1:
+                return middleIndex
+            else:
+                start = middleIndex + 1
+        elif middleData < k:
+            start = middleIndex + 1
+        else:
+            end = middleIndex - 1
+        return self.GetLastK(data, k, start, end)
+    def GetNumberOfK(self, data, k):
+        number = 0
+        if data:
+            first = self.GetFirstK(data, k, 0, len(data) - 1)
+            last = self.GetLastK(data, k, 0, len(data) - 1)
+            if first > -1 and last > -1:
+                number = last - first + 1
+        return number
 # test code
 # pRoot = TreeNode(10)
 # pRoot.left = TreeNode(6)
