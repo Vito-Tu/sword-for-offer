22e78bc4af8185ccd122a2ae8dd3d8bac3796f62
vito <vitotu@qq.com>
2019-07-23 16:59:47 +0800

    update 67

--- debugfile.py
+++ debugfile.py
@@ -9,13 +9,39 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def Add(self, num1, num2):
-        while num2 != 0:
-            sum = num1 ^ num2
-            carry = (num1 & num2) << 1
-            num1 = sum
-            num2 = carry
-        return num1
+    def __init__(self):
+        self.g_nStatus = True
+    def StrToInt(self, s):
+        self.g_nStatus = False
+        num = 0
+        if s:
+            minus = False
+            i = 0
+            if s[i] == '+':
+                i += 1
+            elif s[i] == '-':
+                i += 1
+                minus = True
+            if i < len(s):
+                num = self.StrToIntCore(s, i, minus)
+        return int(num)
+    def StrToIntCore(self, s, i, minus):
+        num = 0
+        while i < len(s):
+            if s[i] >= '0' and s[i] <= '9':
+                flag = -1 if minus else 1
+                num = num * 10 + flag * (ord(s[i]) - ord('0'))
+                if (not minus and num > 0x7fffffff) or (minus and num < -0x80000000):
+                    num = 0
+                    break
+                i += 1
+            else:
+                num = 0
+                break
+        if i == len(s):
+            self.g_nStatus = True
+        return num
+
 # test code
 # pRoot = TreeNode(5)
 # pRoot.left = TreeNode(3)
@@ -26,5 +52,4 @@ class Solution:
 # pRoot.right.right = TreeNode(8)
 a = Solution()
 data = [9,11,8,5,7,12,16,14]
-result = a.Add(5, 17)
-print(result)
+print(a.StrToInt('-123'))
